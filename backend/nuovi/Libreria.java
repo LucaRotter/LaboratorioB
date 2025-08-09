8e4cce30322e75108157161c4daea79899a5fd5d
alegrassii <agrassi6@studenti.uninsubria.it>
2025-08-09 23:37:58 +0200

    creazioine classi Libreria e Libro con aggiunta codice, creazioni classi utente e Valutazione

---
+++ backend/nuovi/Libreria.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Un oggetto della classe <code>Librerie</code> rappresenta
+ * una collezione di libri che personale di un utente, a cui è
+ * associato il nome della libreria scelto dall'utente.
+ * I libri contenuti in questa classe possono essere recensiti 
+ * attraverso valutazioni o libri consigliati assocati
+ * 
+ * @author ProgettoLabA
+ */
+
+//classe Libreria
+public class Libreria{
+    
+    // Attributi della classe Libreria
+    private List<Libro> libreria = new ArrayList<>();
+	private String nomeLibreria;
+	private String nomeUtente;
+
+    //COSTRUTTORI
+	/**
+	 * costruisce un oggetto Librerie attraverso i parametri,
+	 * il primo corrisponde allo Userid, il secondo al nome della
+	 * libreria mentre il terso è l'elenco dei libri
+	 * 
+	 * @param nomeUtente UserId
+	 * @param nomeLibreria nome della libreria
+	 * @param libreria elenco di libri
+	 */
+	public Libreria(String nomeUtente, String nomeLibreria, ArrayList<Libro> libreria) {
+
+		this.nomeUtente = nomeUtente;
+		this.nomeLibreria = nomeLibreria;
+		this.libreria = libreria;
+
+	}
+	
+	/**
+	 * costruisce un oggetto Librerie vuoto
+	 */
+	public Libreria() {
+
+	}
+
+    //METODI
+
+    //metodi getter
+    public String getNomeLibreria() {
+        return nomeLibreria;
+    }
+    public String getNomeUtente() {
+        return nomeUtente;
+    }
+    public List<Libro> getLibreria() {
+        return libreria;
+    }
+
+    /**
+	 * permette all'utente di inserire un libro in una
+	 * libreria personale scelta, aggiungendolo 
+	 * all'elenco di libri presenti
+	 * 
+	 * @param libro libro da aggiungere
+	 */
+    // aggiunge un libro alla libreria
+    public void addLibro(Libro libro) {
+        libreria.add(libro);
+    }
+
+     /**
+	 * permette all'utente di eliminare un libro da una
+	 * libreria personale scelta, rimuovendolo 
+	 * dall'elenco di libri presenti
+	 * 
+	 * @param libro libro da eliminare
+	 */
+    // rimuove un libro dalla libreria
+    public void removeLibro(Libro libro) {
+        libreria.remove(libro);
+    }
+}
---
+++ backend/nuovi/Libro.java
@@ -0,0 +1,73 @@
+//import java.rmi.*;
+
+/**
+ * Un oggetto della classe <code>Libro</code> rappresenta
+ * un libro con titolo, autore, categoria, editore e anno.
+ * Possiede inoltre un elenco di libri consigliati e un
+ * elenco di valutazioni associate al Libro stesso
+ * 
+ * @author ProgettoLabB
+ */
+
+//classe Libro
+public class Libro {
+    private static final long serialVersionUID = 1L;
+
+    // Attributi della classe Libro
+    private String titolo;
+	private String autore;
+	private String categoria;
+	private String editore;
+	private String anno;
+
+    //COSTRUTTORI
+	/**
+	 * Costruisce un oggetto che rappresenta un libro,
+	 * in cui il titolo è il primo parametro, l'autore il secondo,
+	 * la categoria il terzo, l'editore il quarto e l'anno il quinto
+	 * 
+	 * @param titolo il titolo
+	 * @param autore l'autore
+	 * @param categoria la categoria
+	 * @param editore l'editore
+	 * @param anno l'anno
+	 */
+    public Libro(String titolo, String autore, String categoria, String editore, String anno) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.categoria = categoria;
+        this.editore = editore;
+        this.anno = anno;
+    }
+
+    /**
+	 * costruisce un oggetto vuoto
+	 */
+	public Libro() {
+
+	}
+
+    // METODI GETTER
+    public String getTitolo() {
+        return titolo;
+    }
+   
+    public String getAutore() {
+        return autore;
+    }
+    
+    public String getCategoria() {
+        return categoria;
+    }
+    
+    public String getEditore() {
+        return editore;
+    }
+    
+    public String getAnno() {
+        return anno;
+    }
+
+    // METODI SETTER (?)
+
+}
