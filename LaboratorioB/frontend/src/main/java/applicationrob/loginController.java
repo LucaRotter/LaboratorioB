9db1c473383715aad797f32881e24a2855aa1304
sooKyura <davidebilora03pc@gmail.com>
2025-10-09 19:11:57 +0200

    login

--- LaboratorioB/frontend/src/main/java/applicationrob/loginController.java
+++ LaboratorioB/frontend/src/main/java/applicationrob/loginController.java
@@ -17,35 +17,20 @@ public class loginController {
     private void onUserLogin() {
         String email = emailField.getText();
         String pw = pwField.getText();
-
-         if (email.isEmpty() || pw.isEmpty()) {
-            return;
+        //int id_user;  
+        //if(id == -1) { } else { }
         }
-
-        if (checkCredentials(email, pw)) {
-             Model.getIstance().getView().changeToHome();
-        } else {}    
-
-    }
-
     @FXML
     private void changeToRegister() {
     }
 
      @FXML
     public void onBackPressed() {
-        Model.getIstance().getView().changeToHome();
+       // Model.getIstance().getView().changeToHome();
     }
     
     private void showAlert(String title, String message) {
-       
+        
     }
 
-
-    public boolean checkCredentials(String email, String password) {
-    //  String check = "SELECT * FROM nomeTab WHERE email = ? AND password = ?";
-
-
-        return false;
-    }
 }
