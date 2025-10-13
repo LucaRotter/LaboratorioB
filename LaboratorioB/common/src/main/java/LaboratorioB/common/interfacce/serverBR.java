6de754a2e7fe7e9ae0c042859d76eeb73543b131
alegrassii <agrassi6@studenti.uninsubria.it>
2025-10-13 19:00:28 +0200

    .

--- LaboratorioB/common/src/main/java/LaboratorioB/common/interfacce/serverBR.java
+++ LaboratorioB/common/src/main/java/LaboratorioB/common/interfacce/serverBR.java
@@ -20,10 +20,6 @@ public interface serverBR extends Remote {
     public List<Libro> getConsiglio(int id_libro) throws RemoteException;
     public double getVotoMedio(int id_libro) throws RemoteException;
     public List<Libro> cercaLibri(String autore, int anno, String titolo, Ricerca scelta) throws RemoteException;
-    //public boolean modificaPassword(String email, String password) throws RemoteException;
-    //public int modificaEmail(String email, String password, String nuovaEmail) throws RemoteException;
-    //public int modificaNome(String email, String password, String nuovoNome) throws RemoteException;
-    //public int modificaCognome(String email, String password, String nuovoCognome) throws RemoteException;
     public Libreria createLibreria(String nome, int user_id) throws RemoteException;
     public Libreria addLibroLibreria(int id_libro, int id_libreria) throws RemoteException;
     public Libreria removeLibroLibreria(int id_libro, int id_libreria) throws RemoteException;
