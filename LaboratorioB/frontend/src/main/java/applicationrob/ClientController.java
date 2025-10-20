c439a78a75d78c18b639725f497bb00725bb1c25
Aleks13082002 <Aleks.kastratovic@gmail.com>
2025-10-20 13:50:12 +0200

    provacommit

--- LaboratorioB/frontend/src/main/java/applicationrob/ClientController.java
+++ LaboratorioB/frontend/src/main/java/applicationrob/ClientController.java
@@ -28,6 +28,8 @@ public class ClientController implements Initializable{
 			default: MainPage.setCenter(Model.getIstance().getView().getDashboardMain());
 			
 			}
+
+			System.out.println("Changed to: " + newValue);
 		
 		}); 
 	}
