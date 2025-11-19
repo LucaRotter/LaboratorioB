f67c2868b6be3ed6a7f39d40d58d5bc7ee4047a6
alegrassii <agrassi6@studenti.uninsubria.it>
2025-11-19 14:35:15 +0100

    aggiunta doc per enumerativa

--- LaboratorioB/common/src/main/java/LaboratorioB/common/models/Ricerca.java
+++ LaboratorioB/common/src/main/java/LaboratorioB/common/models/Ricerca.java
@@ -1,5 +1,9 @@
 package LaboratorioB.common.models;
 
+/**
+ * Enumerativa che rappresenta i criteri di ricerca disponibili.
+ * @author Laboratorio B
+ */
 public enum Ricerca {
     TITOLO,
     AUTORE,
